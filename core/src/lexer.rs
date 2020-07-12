28120b65fe07905eb88ed8b40c173d4a7ac6f1ac
neverRare <35887047+neverRare@users.noreply.github.com>
2020-07-12 21:30:55 +0800

    laid down few tokens

---
+++ core/src/lexer.rs
@@ -0,0 +1,72 @@
+pub enum Num {
+    UInt(u64),
+    Int(i64),
+    Float(f64),
+}
+pub enum Bracket {
+    Paren,
+    Bracket,
+    Brace,
+}
+pub enum Opening {
+    Open,
+    Close,
+}
+pub enum Keyword {
+    True,
+    False,
+    Null,
+    If,
+    Else,
+    Match,
+    For,
+    Loop,
+    While,
+}
+pub enum Separator {
+    Comma,
+    Semicolon,
+}
+pub enum Operator {
+    Equal,
+    DoubleEqual,
+    NotEqual,
+    Colon,
+    DoubleColon,
+    Dot,
+    DoubleDot,
+    Plus,
+    Minus,
+    Asterisk,
+    Slash,
+    DoubleSlash,
+    Percent,
+    Bang,
+    Amp,
+    Pipe,
+    Caret,
+    Tilde,
+    DoubleAmp,
+    DoublePipe,
+    Greater,
+    Less,
+    DoubleGreater,
+    DoubleLess,
+    GreaterEqual,
+    LessEqual,
+    LeftArrow,
+    RightArrow,
+    RightThickArrow,
+    Question,
+    DoubleQuestion,
+}
+pub enum Token<'a> {
+    Num(Num),
+    Str(Vec<u8>),
+    Char(u8),
+    Keyword(Keyword),
+    Identifier(&'a str),
+    Separator(Separator),
+    Bracket(Opening, Bracket),
+    Operator(Operator),
+}
