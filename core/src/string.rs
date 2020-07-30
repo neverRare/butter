6cc4c1a7786321d0e86959ff8348841c6e6cd71c
neverRare <35887047+neverRare@users.noreply.github.com>
2020-07-30 14:22:30 +0800

    implemented string parser

--- core/src/string.rs
+++ core/src/string.rs
@@ -1,6 +1,7 @@
 struct ParseBytes<'a> {
     quote: char,
     src: &'a str,
+    byte_src: &'a [u8],
     i: usize,
     char_i: usize,
 }
@@ -9,6 +10,7 @@ impl<'a> ParseBytes<'a> {
         Self {
             quote,
             src,
+            byte_src: src.as_bytes(),
             i: 0,
             char_i: 0,
         }
@@ -20,13 +22,82 @@ pub enum ParseError {
 }
 enum ParseResult {
     Yield(u8),
+    Noop,
     Done(usize),
     Error(ParseError, usize, usize),
 }
 impl<'a> Iterator for ParseBytes<'a> {
     type Item = ParseResult;
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        if self.i == self.char_i {
+            let src = &self.src[self.i..];
+            match src.chars().next() {
+                Some(quote) if quote == self.quote => Some(ParseResult::Done(self.i)),
+                Some('\\') => {
+                    let (len, byte) = match src[1..].chars().next() {
+                        Some(code) if code == 'x' || code == 'X' => {
+                            let result = match src.get(2..4) {
+                                Some(code) => u8::from_str_radix(code, 16).map_err(|_| self.i + 4),
+                                None => Err(self.i + 2),
+                            };
+                            match result {
+                                Ok(val) => (4, val),
+                                Err(res) => {
+                                    return Some(ParseResult::Error(
+                                        ParseError::InvalidEscape,
+                                        self.i,
+                                        res,
+                                    ))
+                                }
+                            }
+                        }
+                        Some(code) => {
+                            let byte = match code {
+                                '\\' => b'\\',
+                                '\'' => b'\'',
+                                '"' => b'"',
+                                'n' => b'\n',
+                                'r' => b'\r',
+                                't' => b'\t',
+                                'v' => b'\x30',
+                                '0' => b'\0',
+                                _ => {
+                                    return Some(ParseResult::Error(
+                                        ParseError::InvalidEscape,
+                                        self.i,
+                                        self.i + 2,
+                                    ))
+                                }
+                            };
+                            (2, byte)
+                        }
+                        None => {
+                            return Some(ParseResult::Error(
+                                ParseError::UnterminatedQuote,
+                                0,
+                                self.src.len(),
+                            ))
+                        }
+                    };
+                    self.i += len;
+                    self.char_i += len;
+                    Some(ParseResult::Yield(byte))
+                }
+                Some(val) => {
+                    self.char_i += val.len_utf8();
+                    Some(ParseResult::Noop)
+                }
+                None => Some(ParseResult::Error(
+                    ParseError::UnterminatedQuote,
+                    0,
+                    self.src.len(),
+                )),
+            }
+        } else {
+            let i = self.i;
+            self.i += 1;
+            Some(ParseResult::Yield(self.byte_src[i]))
+        }
     }
 }
 pub fn parse_string(
@@ -39,6 +110,7 @@ pub fn parse_string(
             ParseResult::Yield(val) => vec.push(val),
             ParseResult::Error(err, from, to) => return Err((err, from, to)),
             ParseResult::Done(len) => return Ok((len, vec)),
+            ParseResult::Noop => (),
         }
     }
     unreachable!()
