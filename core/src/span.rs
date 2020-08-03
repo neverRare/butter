32058870d75b3336b8fd4b80e277405f864993d4
neverRare <35887047+neverRare@users.noreply.github.com>
2020-08-03 10:15:10 +0800

    added spans

--- core/src/span.rs
+++ core/src/span.rs
@@ -27,3 +27,22 @@ impl<'a, T> Span<'a, T> {
         }
     }
 }
+pub struct Spans<'a, T, U> {
+    summary: T,
+    src: &'a str,
+    spans: Vec<(U, usize, usize)>,
+}
+impl<'a, T, U> Spans<'a, T, U> {
+    pub fn new(summary: T, src: &'a str, spans: impl IntoIterator<Item = Span<'a, U>>) -> Self {
+        let result = vec![];
+        for Span{src: span_src, note, from, to} in spans {
+            assert_eq!(src, span_src);
+            result.push((note, from, to));
+        }
+        Self {
+            summary,
+            src,
+            spans: result,
+        }
+    }
+}
