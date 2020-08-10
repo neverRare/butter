40f77ade3113da3fe11a92b771b844974f0bcb25
neverRare <35887047+neverRare@users.noreply.github.com>
2020-08-10 12:55:01 +0800

    implemented \xXX escape sequence

--- core/src/lexer/string.rs
+++ core/src/lexer/string.rs
@@ -19,12 +19,41 @@ pub fn parse_string(quote: char, rest: &str) -> (usize, Result<Vec<u8>, StrError
             None => break Err(StrError::Unterminated),
         };
         if let '\\' = first {
-            let escape = match rest[i + first.len_utf8()..].chars().next() {
+            let escape = match rest[i + 1..].chars().next() {
                 Some(ch) => ch,
                 None => break Err(StrError::Unterminated),
             };
             if let 'x' = escape {
-                todo!();
+                let mut len = 0;
+                let mut codes: Vec<u8> = vec![];
+                let code_rest = &rest[i + 2..];
+                let code_chars = code_rest
+                    .char_indices()
+                    .take(2)
+                    .take_while(|(_, ch)| *ch != first && *ch != '\\');
+                for (ind, ch) in code_chars {
+                    len = ind + ch.len_utf8();
+                    match ch.to_digit(16) {
+                        Some(val) => codes.push(val as u8),
+                        None => {
+                            errors.push((
+                                &code_rest[ind..ind + ch.len_utf8()],
+                                EscapeError::InvalidHexChar,
+                            ));
+                        }
+                    }
+                }
+                if codes.len() != 2 {
+                    let slice = if codes.is_empty() {
+                        &rest[i + 1..i + 2]
+                    } else {
+                        &code_rest[..len]
+                    };
+                    errors.push((slice, EscapeError::HexCharTerminated));
+                } else if errors.is_empty() {
+                    vec.push((codes[0] << 4) + codes[1]);
+                }
+                i += 2 + len;
             } else {
                 let byte = match escape {
                     '\\' => b'\\',
