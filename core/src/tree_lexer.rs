7d41d3cd6a102d97f36526f3d91403e6b10d77b8
neverRare <35887047+neverRare@users.noreply.github.com>
2020-08-15 07:37:59 +0800

    laid down structs and enum for tree lexer

---
+++ core/src/tree_lexer.rs
@@ -0,0 +1,41 @@
+use crate::lexer::Bracket;
+use crate::lexer::LexerError;
+use crate::lexer::Token;
+use crate::lexer::TokenSpans;
+pub enum TreeResult<'a> {
+    Token(&'a str, Token<'a>),
+    LexerError(&'a str, LexerError<'a>),
+    TreeError,
+    In(Bracket, TokenTreeSpans<'a>),
+    Out(TokenTreeSpans<'a>),
+}
+pub enum TreeError<'a> {
+    Mismatch((&'a str, Bracket), (&'a str, Bracket)),
+    Unmatched(Vec<(&'a str, Bracket)>),
+}
+pub struct TokenTreeSpans<'a> {
+    tokens: Option<TokenSpans<'a>>,
+    closes: Vec<Bracket>,
+}
+impl<'a> From<TokenSpans<'a>> for TokenTreeSpans<'a> {
+    fn from(val: TokenSpans<'a>) -> Self {
+        Self {
+            tokens: Some(val),
+            closes: vec![],
+        }
+    }
+}
+impl<'a> From<&'a str> for TokenTreeSpans<'a> {
+    fn from(val: &'a str) -> Self {
+        Self {
+            tokens: Some(TokenSpans::new(val)),
+            closes: vec![],
+        }
+    }
+}
+impl<'a> Iterator for TokenTreeSpans<'a> {
+    type Item = TreeResult<'a>;
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
+}
