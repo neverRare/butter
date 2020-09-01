0a0b106fb067fc704ac8c37910d59aed8eca47fc
neverRare <35887047+neverRare@users.noreply.github.com>
2020-09-01 19:32:16 +0800

    add character lexer

--- core/src/lexer/string.rs
+++ core/src/lexer/string.rs
@@ -1,3 +1,90 @@
+use std::num::NonZeroUsize;
+use util::lexer::Lex;
+use util::lexer::MoveState;
+
+enum Char<'a> {
+    Char(char),
+    Byte(u8),
+    Quote,
+    DoubleQuote,
+    Error(&'a str, EscapeError),
+}
+impl<'a> Lex<'a> for Char<'a> {
+    fn lex_first(src: &'a str) -> Option<(MoveState, Self)> {
+        let first = src.chars().next().unwrap();
+        if let '\\' = first {
+            let escape = src[1..].chars().next()?;
+            if let 'x' = escape {
+                let mut len = 0;
+                let mut codes: Vec<u8> = vec![];
+                let code_rest = &src[2..];
+                let code_chars = code_rest
+                    .char_indices()
+                    .take(2)
+                    .take_while(|(_, ch)| *ch != first && *ch != '\\');
+                for (ind, ch) in code_chars {
+                    len = ind + ch.len_utf8();
+                    match ch.to_digit(16) {
+                        Some(val) => codes.push(val as u8),
+                        None => {
+                            return Some((
+                                MoveState::Move(NonZeroUsize::new(2 + len).unwrap()),
+                                Self::Error(
+                                    &code_rest[ind..ind + ch.len_utf8()],
+                                    EscapeError::InvalidHexChar,
+                                ),
+                            ))
+                        }
+                    }
+                }
+                let token = if codes.len() != 2 {
+                    let slice = if codes.is_empty() {
+                        &src[1..2]
+                    } else {
+                        &code_rest[..len]
+                    };
+                    Self::Error(slice, EscapeError::HexCharTerminated)
+                } else {
+                    Self::Byte((codes[0] << 4) + codes[1])
+                };
+                Some((MoveState::Move(NonZeroUsize::new(2 + len).unwrap()), token))
+            } else {
+                let byte = match escape {
+                    '\\' => b'\\',
+                    '\'' => b'\'',
+                    '"' => b'"',
+                    'n' => b'\n',
+                    'r' => b'\r',
+                    't' => b'\t',
+                    'v' => b'\x30',
+                    '0' => b'\0',
+                    _ => {
+                        return Some((
+                            MoveState::Move(NonZeroUsize::new(1 + escape.len_utf8()).unwrap()),
+                            Self::Error(&src[1..1 + escape.len_utf8()], EscapeError::InvalidChar),
+                        ))
+                    }
+                };
+                Some((
+                    MoveState::Move(NonZeroUsize::new(2).unwrap()),
+                    Self::Byte(byte),
+                ))
+            }
+        } else if let '\'' = first {
+            Some((MoveState::Move(NonZeroUsize::new(1).unwrap()), Self::Quote))
+        } else if let '"' = first {
+            Some((
+                MoveState::Move(NonZeroUsize::new(1).unwrap()),
+                Self::DoubleQuote,
+            ))
+        } else {
+            Some((
+                MoveState::Move(NonZeroUsize::new(1).unwrap()),
+                Self::Char(first),
+            ))
+        }
+    }
+}
 #[derive(PartialEq, Eq, Debug)]
 pub enum EscapeError {
     InvalidChar,
@@ -8,6 +95,7 @@ pub enum EscapeError {
 pub enum StrError<'a> {
     InvalidEscape(Vec<(&'a str, EscapeError)>),
     Unterminated,
+    CharNotOne,
 }
 pub fn parse_string(quote: char, rest: &str) -> (usize, Result<Vec<u8>, StrError>) {
     let mut vec: Vec<u8> = vec![];
