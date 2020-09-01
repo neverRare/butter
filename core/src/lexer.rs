ce0de069c764efbc5c1e7b3b6c74716a3309a5b1
neverRare <35887047+neverRare@users.noreply.github.com>
2020-09-02 07:29:12 +0800

    add whitespace lexer

--- core/src/lexer.rs
+++ core/src/lexer.rs
@@ -1,5 +1,15 @@
 use util::lexer::Lex;
 
+struct Whitespace;
+impl<'a> Lex<'a> for Whitespace {
+    fn lex_first(src: &'a str) -> Option<(usize, Self)> {
+        match src.find(|ch: char| !ch.is_whitespace()) {
+            None => Some((src.len(), Self)),
+            Some(0) => None,
+            Some(i) => Some((i, Self)),
+        }
+    }
+}
 struct Ident<'a>(&'a str);
 impl<'a> Lex<'a> for Ident<'a> {
     fn lex_first(src: &'a str) -> Option<(usize, Self)> {
