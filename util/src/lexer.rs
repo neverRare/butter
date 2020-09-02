e44d357d8f31bac0159766d8557bbe9d8e67e4b7
neverRare <35887047+neverRare@users.noreply.github.com>
2020-09-02 09:37:38 +0800

    add lex macro

--- util/src/lexer.rs
+++ util/src/lexer.rs
@@ -43,3 +43,17 @@ where
         }
     }
 }
+#[macro_export]
+macro_rules! match_lex {
+    ($src:expr; $($pat:pat => $expr:expr,)* else $last_pat:pat => $last_expr:expr $(,)?) => {{
+        let src = $src;
+        $(
+            if let Some((step, $pat)) = $crate::Lex::lex_first(src) {
+                Some(lex, $expr)
+            } else
+        )* {
+            let $last_pat = src;
+            $last_expr
+        }
+    }};
+}
