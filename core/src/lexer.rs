443c0518dbc59e0d58bfecfbf4c5856417fb6cc6
neverRare <35887047+neverRare@users.noreply.github.com>
2020-09-02 12:43:05 +0800

    _ now considered as identifier

--- core/src/lexer.rs
+++ core/src/lexer.rs
@@ -28,7 +28,7 @@ impl<'a> Lex<'a> for Comment<'a> {
 struct Ident<'a>(&'a str);
 impl<'a> Lex<'a> for Ident<'a> {
     fn lex_first(src: &'a str) -> Option<(usize, Self)> {
-        match src.find(|ch: char| !ch.is_alphanumeric()) {
+        match src.find(|ch: char| ch != '_' && !ch.is_alphanumeric()) {
             None => Some((src.len(), Self(src))),
             Some(0) => None,
             Some(i) => Some((i, Self(&src[..i]))),
