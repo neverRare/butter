0fa19ed999c7f5a8dd19923e9075aec3aaf024aa
neverRare <35887047+neverRare@users.noreply.github.com>
2020-09-02 12:41:00 +0800

    fixed comment lexer, shouldn't consider -- as content

--- core/src/lexer.rs
+++ core/src/lexer.rs
@@ -16,7 +16,7 @@ impl<'a> Lex<'a> for Comment<'a> {
     fn lex_first(src: &'a str) -> Option<(usize, Self)> {
         if let Some("--") = src.get(..2) {
             match src[2..].find('\n') {
-                None => Some((src.len(), Self(src))),
+                None => Some((src.len(), Self(&src[2..]))),
                 Some(0) => None,
                 Some(i) => Some((i + 3, Self(&src[2..i + 2]))),
             }
