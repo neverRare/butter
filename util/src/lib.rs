e75432766fc9b975fbfdbd527a390c0abbda75b3
neverRare <35887047+neverRare@users.noreply.github.com>
2020-09-02 09:50:24 +0800

    move macro to root crate

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,3 +1,16 @@
 pub mod lexer;
 
-pub enum Void {}
+#[macro_export]
+macro_rules! match_lex {
+    ($src:expr; $($pat:pat => $expr:expr,)* else let $last_pat:pat => $last_expr:expr $(,)?) => {{
+        let src = $src;
+        $(
+            if let Some((step, $pat)) = $crate::lexer::Lex::lex_first(src) {
+                Some(lex, $expr)
+            } else
+        )* {
+            let $last_pat = src;
+            $last_expr
+        }
+    }};
+}
