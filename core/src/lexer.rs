2d7292cc77335f2fa14f08a8e0ab3ead445445f3
neverRare <35887047+neverRare@users.noreply.github.com>
2020-09-02 09:19:40 +0800

    implement string lexer

--- core/src/lexer.rs
+++ core/src/lexer.rs
@@ -239,6 +239,40 @@ impl<'a> Lex<'a> for Num<'a> {
         }
     }
 }
+enum Str<'a> {
+    Str(&'a str),
+    Char(&'a str),
+    Unterminated(char),
+}
+impl<'a> Lex<'a> for Str<'a> {
+    fn lex_first(src: &'a str) -> Option<(usize, Self)> {
+        let mut chars = src.char_indices();
+        let (_, first) = chars.next().unwrap();
+        if let '\'' | '"' = first {
+            let mut escaping = false;
+            for (i, ch) in chars {
+                if escaping {
+                    escaping = false;
+                    continue;
+                } else if let '\\' = ch {
+                    escaping = true;
+                    continue;
+                } else if first == ch {
+                    let content = &src[1..i];
+                    let token = match first {
+                        '\'' => Self::Char(content),
+                        '"' => Self::Str(content),
+                        _ => unreachable!(),
+                    };
+                    return Some((i + 2, token));
+                }
+            }
+            Some((src.len(), Self::Unterminated(first)))
+        } else {
+            None
+        }
+    }
+}
 #[derive(PartialEq, Debug)]
 pub enum Token<'a> {
     Whitespace,
