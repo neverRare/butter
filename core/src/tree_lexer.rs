aac13bef6941a6413026d58d32409e1218621713
neverRare <35887047+neverRare@users.noreply.github.com>
2020-09-02 16:13:59 +0800

    add tree and tree iter

--- core/src/tree_lexer.rs
+++ core/src/tree_lexer.rs
@@ -7,6 +7,32 @@ use crate::lexer::Token as SrcToken;
 use util::lexer::Lex;
 use util::span::Span;
 
+pub enum Token<'a> {
+    Num(&'a str),
+    Str(&'a str),
+    Char(&'a str),
+    Keyword(Keyword),
+    Identifier(&'a str),
+    Separator(Separator),
+    Operator(Operator),
+}
+pub enum Error<'a> {
+    UnterminatedQuote(char, &'a str),
+    InvalidToken(&'a str),
+    Mismatch((&'a str, Bracket), (&'a str, Bracket)),
+    Unexpected(&'a str, Bracket),
+    Unmatched(&'a str, Bracket),
+}
+pub enum TokenTree<'a, 'b> {
+    Token(&'b Token<'a>),
+    Tree(&'b Bracket, Tree<'a, 'b>),
+    Error(&'b Error<'a>),
+}
+enum Node<'a> {
+    Token(Token<'a>),
+    Tree(Bracket, usize),
+    Error(Error<'a>),
+}
 pub struct BigTree<'a>(Vec<(&'a str, Node<'a>)>);
 impl<'a> BigTree<'a> {
     pub fn new(src: &'a str) -> Self {
@@ -70,25 +96,50 @@ impl<'a> BigTree<'a> {
         }
         Self(current)
     }
+    pub fn tree<'b>(&'b self) -> Tree<'a, 'b> {
+        self.into()
+    }
 }
-enum Node<'a> {
-    Token(Token<'a>),
-    Tree(Bracket, usize),
-    Error(Error<'a>),
+#[derive(Clone, Copy)]
+pub struct Tree<'a, 'b>(&'b [(&'a str, Node<'a>)]);
+impl<'a, 'b> From<&'b BigTree<'a>> for Tree<'a, 'b> {
+    fn from(BigTree(vec): &'b BigTree<'a>) -> Self {
+        Self(vec)
+    }
 }
-pub enum Token<'a> {
-    Num(&'a str),
-    Str(&'a str),
-    Char(&'a str),
-    Keyword(Keyword),
-    Identifier(&'a str),
-    Separator(Separator),
-    Operator(Operator),
+impl<'a, 'b> IntoIterator for Tree<'a, 'b> {
+    type Item = (&'a str, TokenTree<'a, 'b>);
+    type IntoIter = TreeIter<'a, 'b>;
+    fn into_iter(self) -> Self::IntoIter {
+        TreeIter { src: self.0 }
+    }
 }
-pub enum Error<'a> {
-    UnterminatedQuote(char, &'a str),
-    InvalidToken(&'a str),
-    Mismatch((&'a str, Bracket), (&'a str, Bracket)),
-    Unexpected(&'a str, Bracket),
-    Unmatched(&'a str, Bracket),
+pub struct TreeIter<'a, 'b> {
+    src: &'b [(&'a str, Node<'a>)],
+}
+impl<'a, 'b> Iterator for TreeIter<'a, 'b> {
+    type Item = (&'a str, TokenTree<'a, 'b>);
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.src.is_empty() {
+            None
+        } else {
+            let (span, node) = &self.src[0];
+            let token = match node {
+                Node::Token(token) => {
+                    self.src = &self.src[1..];
+                    TokenTree::Token(token)
+                }
+                Node::Tree(bracket, len) => {
+                    let tree = Tree(&self.src[1 + len..]);
+                    self.src = &self.src[1..];
+                    TokenTree::Tree(bracket, tree)
+                }
+                Node::Error(msg) => {
+                    self.src = &self.src[1..];
+                    TokenTree::Error(msg)
+                }
+            };
+            Some((span, token))
+        }
+    }
 }
