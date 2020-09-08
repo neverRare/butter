4ed7e96cd0d049d6b8eeb0b40d3f32437ebe606a
neverRare <35887047+neverRare@users.noreply.github.com>
2020-09-08 15:25:56 +0800

    more helper methods

--- util/src/tree.rs
+++ util/src/tree.rs
@@ -1,6 +1,7 @@
 use std::fmt;
 use std::fmt::Debug;
 use std::fmt::Formatter;
+use std::iter::FusedIterator;
 use std::mem::swap;
 use std::ops::Deref;
 use std::ops::DerefMut;
@@ -26,6 +27,9 @@ impl<T> BigTree<T> {
     pub fn new() -> Self {
         Self::default()
     }
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self(Vec::with_capacity(capacity))
+    }
     pub fn branch(leaf: T, branch: Self) -> Self {
         let Self(mut branch) = branch;
         let mut vec = vec![Node {
@@ -44,6 +48,28 @@ impl<T> BigTree<T> {
     pub fn append(&mut self, branch: &mut Self) {
         self.0.append(&mut branch.0)
     }
+    pub fn into_first(self) -> Option<(T, Self)> {
+        let mut vec = self.0;
+        if vec.is_empty() {
+            None
+        } else {
+            let first = vec.remove(0);
+            let len = first.len;
+            vec.truncate(len);
+            Some((first.content, Self(vec)))
+        }
+    }
+    pub fn into_first_and_rest(self) -> Option<(T, Self, Self)> {
+        let mut vec = self.0;
+        if vec.is_empty() {
+            None
+        } else {
+            let first = vec.remove(0);
+            let len = first.len;
+            let rest = vec.split_off(len);
+            Some((first.content, Self(vec), Self(rest)))
+        }
+    }
 }
 impl<T> Deref for BigTree<T> {
     type Target = Tree<T>;
@@ -60,7 +86,7 @@ impl<T> IntoIterator for BigTree<T> {
     type Item = (T, Self);
     type IntoIter = TreeIntoIter<T>;
     fn into_iter(self) -> Self::IntoIter {
-        TreeIntoIter(self.0)
+        TreeIntoIter(self)
     }
 }
 impl<T: Debug> Debug for BigTree<T> {
@@ -83,6 +109,57 @@ impl<T> Tree<T> {
     pub fn iter(&self) -> TreeIter<T> {
         self.into_iter()
     }
+    pub fn first(&self) -> Option<(&T, &Self)> {
+        let arr = &self.0;
+        if arr.is_empty() {
+            None
+        } else {
+            let first = &arr[0];
+            let children = &arr[1..1 + first.len];
+            Some((&first.content, Self::from_slice(children)))
+        }
+    }
+    pub fn first_and_rest(&self) -> Option<(&T, &Self, &Self)> {
+        let arr = &self.0;
+        if arr.is_empty() {
+            None
+        } else {
+            let first = &arr[0];
+            let children = &arr[1..1 + first.len];
+            let rest = &arr[1 + first.len..];
+            Some((
+                &first.content,
+                Self::from_slice(children),
+                Self::from_slice(rest),
+            ))
+        }
+    }
+    pub fn first_mut(&mut self) -> Option<(&mut T, &mut Self)> {
+        let arr = &mut self.0;
+        if arr.is_empty() {
+            None
+        } else {
+            let (first, rest) = arr.split_at_mut(1);
+            let first = &mut first[0];
+            let children = &mut rest[..first.len];
+            Some((&mut first.content, Self::from_mut_slice(children)))
+        }
+    }
+    pub fn first_and_rest_mut(&mut self) -> Option<(&mut T, &mut Self, &mut Self)> {
+        let arr = &mut self.0;
+        if arr.is_empty() {
+            None
+        } else {
+            let (first, rest) = arr.split_at_mut(1);
+            let first = &mut first[0];
+            let (children, rest) = rest.split_at_mut(first.len);
+            Some((
+                &mut first.content,
+                Self::from_mut_slice(children),
+                Self::from_mut_slice(rest),
+            ))
+        }
+    }
 }
 impl<'a, T> Default for &'a Tree<T> {
     fn default() -> Self {
@@ -98,7 +175,7 @@ impl<'a, T> IntoIterator for &'a Tree<T> {
     type Item = (&'a T, &'a Tree<T>);
     type IntoIter = TreeIter<'a, T>;
     fn into_iter(self) -> Self::IntoIter {
-        TreeIter(&self.0)
+        TreeIter(self)
     }
 }
 impl<T: Debug> Debug for Tree<T> {
@@ -106,42 +183,39 @@ impl<T: Debug> Debug for Tree<T> {
         fmt.debug_list().entries(self.iter()).finish()
     }
 }
-pub struct TreeIter<'a, T>(&'a [Node<T>]);
+pub struct TreeIter<'a, T>(&'a Tree<T>);
 impl<'a, T> Iterator for TreeIter<'a, T> {
     type Item = (&'a T, &'a Tree<T>);
     fn next(&mut self) -> Option<Self::Item> {
-        let arr = self.0;
-        if arr.is_empty() {
-            None
-        } else {
-            let first = &arr[0];
-            let len = first.len;
-            self.0 = &arr[1 + len..];
-            Some((&first.content, Tree::from_slice(&arr[1..1 + len])))
-        }
+        self.0.first_and_rest().map(|(content, children, rest)| {
+            self.0 = rest;
+            (content, children)
+        })
     }
     fn size_hint(&self) -> (usize, Option<usize>) {
-        let len = self.0.len();
+        let len = (self.0).0.len();
         (1.min(len), Some(len))
     }
 }
-pub struct TreeIntoIter<T>(Vec<Node<T>>);
+impl<'a, T> FusedIterator for TreeIter<'a, T> {}
+pub struct TreeIntoIter<T>(BigTree<T>);
 impl<T> Iterator for TreeIntoIter<T> {
     type Item = (T, BigTree<T>);
     fn next(&mut self) -> Option<Self::Item> {
-        let self_arr = &mut self.0;
-        if self_arr.is_empty() {
+        let self_vec = &mut (self.0).0;
+        if self_vec.is_empty() {
             None
         } else {
-            let first = self_arr.remove(0);
-            let len = first.len;
-            let mut other_arr = self_arr.split_off(len);
-            swap(self_arr, &mut other_arr);
-            Some((first.content, BigTree(other_arr)))
+            let mut vec = vec![];
+            swap(self_vec, &mut vec);
+            let (content, children, rest) = BigTree(vec).into_first_and_rest().unwrap();
+            *self_vec = rest.0;
+            Some((content, children))
         }
     }
     fn size_hint(&self) -> (usize, Option<usize>) {
-        let len = self.0.len();
+        let len = (self.0).0.len();
         (1.min(len), Some(len))
     }
 }
+impl<T> FusedIterator for TreeIntoIter<T> {}
