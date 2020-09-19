683a303f928368cd0c7edf484e0100e352337333
neverRare <35887047+neverRare@users.noreply.github.com>
2020-09-19 20:23:30 +0800

    initial definition of parser trait

---
+++ util/src/parser.rs
@@ -0,0 +1,10 @@
+use crate::tree::Tree;
+
+trait Parser: Sized {
+    type Tokens: Iterator;
+    type Error;
+    fn error_node() -> Self;
+    fn simple_node(tokens: &mut Self::Tokens) -> Result<Tree<Self>, Self::Error>;
+    fn partial_node(tokens: &mut Self::Tokens) -> Result<Tree<Self>, Self::Error>;
+    fn precedence(tree: &Self) -> u32;
+}
