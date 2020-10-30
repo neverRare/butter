e418ae6e8bdd83e501f9c20c220f1c3f197e0134
neverRare <35887047+neverRare@users.noreply.github.com>
2020-10-30 10:30:33 +0800

    restructure parser trait

--- util/src/parser.rs
+++ util/src/parser.rs
@@ -1,10 +1,18 @@
 use crate::tree_vec::Tree;
 
-trait Parser: Sized {
-    type Tokens: Iterator;
-    type Error;
+// TODO: better error handling
+trait Parser<'a>: Sized {
+    type Token;
+    type Tokens: Iterator<Item = Self::Token> + From<&'a str>;
     fn error_node() -> Self;
-    fn simple_node(tokens: &mut Self::Tokens) -> Result<Tree<Self>, Self::Error>;
-    fn partial_node(tokens: &mut Self::Tokens) -> Result<Tree<Self>, Self::Error>;
-    fn precedence(tree: &Self) -> u32;
+    fn prefix_parse(prefix: Self::Token, tokens: &mut Self::Tokens) -> Tree<Self>;
+    fn infix_parse(infix: Self::Token, tokens: &mut Self::Tokens) -> Tree<Self>;
+    fn prefix_precedence(token: &Self::Token) -> Option<u32>;
+    fn infix_precedence(token: &Self::Token) -> Option<u32>;
+    fn partial_parse(tokens: &mut Self::Tokens, precedence: u32) -> Tree<Self> {
+        todo!();
+    }
+    fn parse(src: &'a str) -> Tree<Self> {
+        Self::partial_parse(&mut src.into(), 0)
+    }
 }
