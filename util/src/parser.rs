c55cfe896b87de4f06a69aa49bee374b867aebe3
neverRare <35887047+neverRare@users.noreply.github.com>
2020-10-31 07:56:34 +0800

    restructure Parser trait yet again

--- util/src/parser.rs
+++ util/src/parser.rs
@@ -1,18 +1,39 @@
 use crate::tree_vec::Tree;
+use std::iter::Peekable;
 
 // TODO: better error handling
+// TODO: test
 trait Parser<'a>: Sized {
     type Token;
-    type Tokens: Iterator<Item = Self::Token> + From<&'a str>;
     fn error_node() -> Self;
-    fn prefix_parse(prefix: Self::Token, tokens: &mut Self::Tokens) -> Tree<Self>;
-    fn infix_parse(infix: Self::Token, tokens: &mut Self::Tokens) -> Tree<Self>;
-    fn prefix_precedence(token: &Self::Token) -> Option<u32>;
+    fn prefix_parse(
+        prefix: Self::Token,
+        tokens: &mut Peekable<impl Iterator<Item = Self::Token>>,
+    ) -> Tree<Self>;
+    fn infix_parse(
+        left_node: Tree<Self>,
+        infix: Self::Token,
+        tokens: &mut Peekable<impl Iterator<Item = Self::Token>>,
+    ) -> Tree<Self>;
     fn infix_precedence(token: &Self::Token) -> Option<u32>;
-    fn partial_parse(tokens: &mut Self::Tokens, precedence: u32) -> Tree<Self> {
-        todo!();
-    }
-    fn parse(src: &'a str) -> Tree<Self> {
-        Self::partial_parse(&mut src.into(), 0)
+    fn partial_parse(
+        tokens: &mut Peekable<impl Iterator<Item = Self::Token>>,
+        precedence: u32,
+    ) -> Tree<Self> {
+        let token = match tokens.next() {
+            Some(node) => node,
+            None => return Tree::new(Self::error_node()),
+        };
+        let mut node = Self::prefix_parse(token, tokens);
+        while let Some(token) = tokens.next() {
+            if Self::infix_precedence(&token)
+                .map(|num| num < precedence)
+                .unwrap_or(true)
+            {
+                break;
+            }
+            node = Self::infix_parse(node, token, tokens);
+        }
+        node
     }
 }
