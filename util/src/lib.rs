01781bdb2f5b64dee8f1bcd5e32bed4624c4ef3b
neverRare <35887047+neverRare@users.noreply.github.com>
2020-11-01 10:08:31 +0800

    add mini_fn macro

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -3,8 +3,8 @@
 pub mod lexer;
 pub mod parser;
 pub mod span;
-pub mod tree_vec;
 pub mod tree;
+pub mod tree_vec;
 
 #[macro_export]
 macro_rules! assert_iter {
@@ -16,5 +16,41 @@ macro_rules! assert_iter {
             std::assert_eq!(Iterator::next(&mut iter), Option::Some($item));
         )*
         std::assert_eq!(Iterator::next(&mut iter), Option::None);
+    }};
+}
+#[macro_export]
+macro_rules! mini_fn {
+    (($a:expr $(,)?); $($path:path,)* => else $else:expr $(,)?) => {{
+        let a = $a;
+        $(
+            if let Some(val) = $path(a) {
+                val
+            } else
+        )* {
+            $else
+        }
+    }};
+    (($a:expr, $b:expr $(,)?); $($path:path,)* => else $else:expr $(,)?) => {{
+        let a = $a;
+        let b = $b;
+        $(
+            if let Some(val) = $path(a, b) {
+                val
+            } else
+        )* {
+            $else
+        }
+    }};
+    (($a:expr, $b:expr, $c: expr $(,)?); $($path:path,)* => else $else:expr $(,)?) => {{
+        let a = $a;
+        let b = $b;
+        let c = $c;
+        $(
+            if let Some(val) = $path(a, b, c) {
+                val
+            } else
+        )* {
+            $else
+        }
     }};
 }
