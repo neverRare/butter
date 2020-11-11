77dc1f1e98c1ea9d274d9050b34dd06423da39cd
neverRare <35887047+neverRare@users.noreply.github.com>
2020-11-11 08:48:39 +0800

    update span

--- util/src/span.rs
+++ util/src/span.rs
@@ -1,22 +1,34 @@
-#[derive(Clone, Copy)]
+// TODO: better debug implementation
+#[derive(Clone, Copy, Debug)]
 struct WholeSpan<'a> {
     src: &'a str,
     start: usize,
     end: usize,
 }
-#[derive(Clone, Copy)]
+impl<'a> PartialEq for WholeSpan<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        std::ptr::eq(self.src, other.src) && self.start == other.start && self.end == other.end
+    }
+}
+impl<'a> Eq for WholeSpan<'a> {}
+impl<'a> WholeSpan<'a> {
+    fn span(self) -> &'a str {
+        unsafe { self.src.get_unchecked(self.start..self.end) }
+    }
+}
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
 enum SpanOrEof<'a> {
     Span(WholeSpan<'a>),
     Eof,
 }
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub struct Span<'a>(SpanOrEof<'a>);
 impl<'a> Span<'a> {
     pub fn eof() -> Self {
         Self(SpanOrEof::Eof)
     }
     pub fn from_str(src: &'a str, span: &'a str) -> Self {
-        debug_assert!(
+        assert!(
             src.as_ptr() <= span.as_ptr()
                 && src.as_ptr() as usize + src.len() >= span.as_ptr() as usize + span.len()
         );
@@ -36,15 +48,16 @@ impl<'a> Span<'a> {
             }
             (SpanOrEof::Span(start), SpanOrEof::Eof) => (start.src, start.start, start.src.len()),
             (SpanOrEof::Span(start), SpanOrEof::Span(end)) => {
-                assert!(std::ptr::eq(start.src, end.src));
+                assert!(std::ptr::eq(start.src, end.src) && start.start < end.end);
                 (start.src, start.start, end.end)
             }
         };
-        Self::from_str(src, &src[start..end])
+        let span = unsafe { src.get_unchecked(start..end) };
+        Self::from_str(src, span)
     }
     pub fn src_and_span(self) -> Option<(&'a str, &'a str)> {
         match self.0 {
-            SpanOrEof::Span(span) => Some((span.src, &span.src[span.start..span.end])),
+            SpanOrEof::Span(span) => Some((span.src, span.span())),
             SpanOrEof::Eof => None,
         }
     }
@@ -52,9 +65,34 @@ impl<'a> Span<'a> {
         match self.0 {
             SpanOrEof::Span(span) => {
                 assert!(std::ptr::eq(span.src, src));
-                &src[span.start..span.end]
+                span.span()
             }
-            SpanOrEof::Eof => &src[src.len()..],
+            SpanOrEof::Eof => unsafe { src.get_unchecked(src.len()..) },
         }
+    }
+}
+#[cfg(test)]
+mod test {
+    use crate::span::Span;
+    #[test]
+    fn from_into() {
+        let src = "test";
+        let span_src = &src[1..2];
+        let eof_src = &src[4..];
+        let span = Span::from_str(src, span_src);
+        let eof = Span::eof();
+        assert!(std::ptr::eq(span.span_with_src(src), span_src));
+        assert!(std::ptr::eq(eof.span_with_src(src), eof_src));
+    }
+    #[test]
+    fn up_to() {
+        let src = "test";
+        let left_src = &src[1..2];
+        let right_src = &src[2..3];
+        let result_src = &src[1..3];
+        let left = Span::from_str(src, left_src);
+        let right = Span::from_str(src, right_src);
+        let result = Span::from_str(src, result_src);
+        assert_eq!(left.up_to(right), result);
     }
 }
