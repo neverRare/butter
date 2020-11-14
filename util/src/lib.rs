8cbe317e1b725988447db6adab1d1863383d6b3a
neverRare <35887047+neverRare@users.noreply.github.com>
2020-11-14 19:43:25 +0800

    made aggregate_error generic

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -5,14 +5,14 @@ pub mod lexer;
 pub mod parser;
 pub mod tree_vec;
 
-pub fn aggregate_error<T, U, E>(
-    left: Result<T, Vec<E>>,
-    right: Result<U, Vec<E>>,
-) -> Result<(T, U), Vec<E>> {
+pub fn aggregate_error<T, U, E, A>(left: Result<T, E>, right: Result<U, E>) -> Result<(T, U), E>
+where
+    E: Extend<A> + IntoIterator<Item = A>,
+{
     match (left, right) {
         (Ok(left), Ok(right)) => Ok((left, right)),
-        (Err(mut left), Err(mut right)) => {
-            left.append(&mut right);
+        (Err(mut left), Err(right)) => {
+            left.extend(right);
             Err(left)
         }
         (Err(err), _) | (_, Err(err)) => Err(err),
