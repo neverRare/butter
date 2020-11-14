0d548e437c7d83009f2d889da8a5288312fc6919
neverRare <35887047+neverRare@users.noreply.github.com>
2020-11-14 18:18:22 +0800

    improve string lexer

--- core/src/lexer/string.rs
+++ core/src/lexer/string.rs
@@ -12,22 +12,20 @@ impl<'a> Lex<'a> for Str<'a> {
         if let '\'' | '"' = first {
             let mut escaping = false;
             for (i, ch) in chars {
-                if let '\n' = ch {
-                    return Some((i + 1, Self::Unterminated));
-                } else if escaping {
-                    escaping = false;
-                    continue;
-                } else if let '\\' = ch {
-                    escaping = true;
-                    continue;
-                } else if first == ch {
-                    let content = &src[1..i];
-                    let token = match first {
-                        '\'' => Self::Char(content),
-                        '"' => Self::Str(content),
-                        _ => unreachable!(),
-                    };
-                    return Some((i + 1, token));
+                match ch {
+                    '\n' => return Some((i + 1, Self::Unterminated)),
+                    _ if escaping => escaping = false,
+                    '\\' => escaping = true,
+                    ch if ch == first => {
+                        let content = &src[1..i];
+                        let token = match first {
+                            '\'' => Self::Char(content),
+                            '"' => Self::Str(content),
+                            _ => unsafe { std::hint::unreachable_unchecked() },
+                        };
+                        return Some((i + 1, token));
+                    }
+                    _ => {}
                 }
             }
             Some((src.len(), Self::Unterminated))
