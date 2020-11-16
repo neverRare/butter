f17b5cb97646413bd86d05ca92d42c2b030422d2
neverRare <35887047+neverRare@users.noreply.github.com>
2020-11-16 15:32:43 +0800

    implement integer lexer

--- core/src/lexer/integer.rs
+++ core/src/lexer/integer.rs
@@ -7,9 +7,40 @@ pub enum Radix {
     Oct,
     Bin,
 }
+impl Radix {
+    fn from_indicator(src: &str) -> Option<Self> {
+        match src {
+            "0x" | "0X" => Some(Self::Hex),
+            "0o" | "0O" => Some(Self::Oct),
+            "0b" | "0B" => Some(Self::Bin),
+            _ => None,
+        }
+    }
+    fn valid_digit(self, ch: char) -> bool {
+        match self {
+            Self::Hex => matches!(ch, '_' | '0'..='9' | 'a'..='f' | 'A'..='F'),
+            Self::Dec => matches!(ch, '_' | '0'..='9'),
+            Self::Oct => matches!(ch, '_' | '0'..='7'),
+            Self::Bin => matches!(ch, '_' | '0' | '1'),
+        }
+    }
+}
 pub struct Int<'a>(pub Radix, pub &'a str);
 impl<'a> Lex<'a> for Int<'a> {
     fn lex_first(src: &'a str) -> Option<(usize, Self)> {
-        todo!();
+        let (radix, pos) = if let Some(radix) = src.get(..2).and_then(Radix::from_indicator) {
+            (radix, 2)
+        } else {
+            (Radix::Dec, 0)
+        };
+        let content = &src[pos..];
+        for (ind, ch) in content.char_indices() {
+            if ch != '_' || !ch.is_alphanumeric() {
+                return Some((ind + pos, Self(radix, &content[..ind])));
+            } else if !radix.valid_digit(ch) {
+                return None;
+            }
+        }
+        Some((src.len(), Self(radix, content)))
     }
 }
