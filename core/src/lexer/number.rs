cc5afcfc61dbfc6188de61155bf9115c90b4abc9
neverRare <35887047+neverRare@users.noreply.github.com>
2020-11-19 17:08:41 +0800

    update number lexer

--- core/src/lexer/number.rs
+++ core/src/lexer/number.rs
@@ -4,7 +4,9 @@ pub struct Num;
 impl<'a> Lex<'a> for Num {
     fn lex_first(src: &'a str) -> Option<(usize, Self)> {
         if let Some('0'..='9') = src.chars().next() {
-            let pos = src[1..].find(|ch: char| !ch.is_alphanumeric()).unwrap_or(0);
+            let pos = src[1..]
+                .find(|ch: char| ch != '_' && !ch.is_alphanumeric())
+                .unwrap_or(0);
             Some((pos + 1, Self))
         } else {
             None
