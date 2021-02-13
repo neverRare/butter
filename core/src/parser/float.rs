2bb3b6ce884693cb54a5befcab31a8f849965cca
neverRare <35887047+neverRare@users.noreply.github.com>
2021-02-13 08:19:05 +0800

    update number parser and add more test

--- core/src/parser/float.rs
+++ core/src/parser/float.rs
@@ -8,34 +8,27 @@ pub fn parse_float(float: Float) -> Option<f64> {
         Sign::Minus => -1,
     };
     let exp = exp_sign.checked_mul(parse_i32(10, float.exp.as_bytes())?)?;
-    if f64::MIN_10_EXP < exp && exp < f64::MAX_10_EXP {
-        Some(
-            (exp..)
-                .map(|exp| 10_f64.powi(exp))
-                .zip(
-                    float
-                        .whole
-                        .as_bytes()
-                        .iter()
-                        .filter(|ch| **ch != b'_')
-                        .rev()
-                        .map(|ch| *ch as u32 - b'0' as u32),
-                )
-                .map(|(place, digit)| place * digit as f64)
-                .fold(0.00, |left, right| left + right)
-                + (1..)
-                    .map(|i| 10_f64.powi(exp - i))
-                    .zip(
-                        float
-                            .decimal
-                            .as_bytes()
-                            .iter()
-                            .filter(|ch| **ch != b'_')
-                            .map(|ch| *ch as u32 - b'0' as u32),
-                    )
-                    .map(|(place, digit)| place * digit as f64)
-                    .fold(0.00, |left, right| left + right),
-        )
+    if f64::MIN_10_EXP <= exp && exp <= f64::MAX_10_EXP {
+        let whole = float
+            .whole
+            .as_bytes()
+            .iter()
+            .filter(|ch| **ch != b'_')
+            .rev()
+            .map(|ch| *ch as u32 - b'0' as u32)
+            .zip((exp..).map(|exp| 10_f64.powi(exp)))
+            .map(|(digit, place)| place * digit as f64)
+            .fold(0.00, |left, right| left + right);
+        let decimal = float
+            .decimal
+            .as_bytes()
+            .iter()
+            .filter(|ch| **ch != b'_')
+            .map(|ch| *ch as u32 - b'0' as u32)
+            .zip((1..).map(|i| 10_f64.powi(exp - i)))
+            .map(|(digit, place)| place * digit as f64)
+            .fold(0.00, |left, right| left + right);
+        Some(whole + decimal)
     } else {
         None
     }
@@ -65,5 +58,29 @@ mod test {
         })
         .unwrap();
         assert!(compare_float(float, 1.2e-3));
+        let float = parse_float(Float {
+            whole: "",
+            decimal: "2",
+            exp_sign: Sign::Plus,
+            exp: "3",
+        })
+        .unwrap();
+        assert!(compare_float(float, 0.2e3));
+        let float = parse_float(Float {
+            whole: "1",
+            decimal: "",
+            exp_sign: Sign::Plus,
+            exp: "3",
+        })
+        .unwrap();
+        assert!(compare_float(float, 1e3));
+        let float = parse_float(Float {
+            whole: "1",
+            decimal: "2",
+            exp_sign: Sign::Plus,
+            exp: "",
+        })
+        .unwrap();
+        assert!(compare_float(float, 1.2));
     }
 }
--- core/src/parser/integer.rs
+++ core/src/parser/integer.rs
@@ -1,20 +1,17 @@
 macro_rules! gen_parser {
     ($name:ident, $type:ty $(,)?) => {
         pub fn $name(radix: $type, src: &[u8]) -> Option<$type> {
-            (0..)
-                .map(|i| radix.checked_pow(i))
-                .zip(
-                    src.iter()
-                        .filter(|ch| **ch != b'_')
-                        .rev()
-                        .map(|ch| match ch {
-                            b'0'..=b'9' => *ch as $type - b'0' as $type,
-                            b'a'..=b'z' => *ch as $type + 10 - b'a' as $type,
-                            b'A'..=b'Z' => *ch as $type + 10 - b'A' as $type,
-                            _ => panic!(),
-                        }),
-                )
-                .map(|(place, digit)| place.and_then(|place| place.checked_mul(digit)))
+            src.iter()
+                .filter(|ch| **ch != b'_')
+                .rev()
+                .map(|ch| match ch {
+                    b'0'..=b'9' => *ch as $type - b'0' as $type,
+                    b'a'..=b'z' => *ch as $type + 10 - b'a' as $type,
+                    b'A'..=b'Z' => *ch as $type + 10 - b'A' as $type,
+                    _ => panic!(),
+                })
+                .zip((0..).map(|i| radix.checked_pow(i)))
+                .map(|(digit, place)| place.and_then(|place| place.checked_mul(digit)))
                 .fold(Some(0), |left, right| match (left, right) {
                     (Some(left), Some(right)) => left.checked_add(right),
                     _ => None,
