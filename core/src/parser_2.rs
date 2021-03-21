3d4ab2c611f6d388e4b5ea85683dd3b7204e6dae
neverRare <35887047+neverRare@users.noreply.github.com>
2021-03-21 12:59:34 +0800

    implement comment parser

--- core/src/parser_2.rs
+++ core/src/parser_2.rs
@@ -1,1 +1,16 @@
+use combine::parser::char::string;
+use combine::parser::range::take_while;
+use combine::ParseError;
+use combine::Parser;
+use combine::RangeStream;
+
 mod ident_keyword;
+
+pub fn comment<'a, I>() -> impl Parser<I, Output = &'a str>
+where
+    I: RangeStream<Token = char, Range = &'a str>,
+    I::Error: ParseError<I::Token, I::Range, I::Position>,
+{
+    (string("--"), take_while(|ch: char| ch != '\n'))
+        .map(|(_, content): (&str, &str)| content.trim())
+}
