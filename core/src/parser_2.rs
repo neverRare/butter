88caa91f3a99865f754b3b35b1deee3311d97878
neverRare <35887047+neverRare@users.noreply.github.com>
2021-03-25 07:43:27 +0800

    update comment and insignificants parser

--- core/src/parser_2.rs
+++ core/src/parser_2.rs
@@ -1,16 +1,24 @@
+use combine::parser::char::spaces;
 use combine::parser::char::string;
 use combine::parser::range::take_while;
+use combine::skip_many;
 use combine::ParseError;
 use combine::Parser;
 use combine::RangeStream;
 
 mod ident_keyword;
 
-pub fn comment<'a, I>() -> impl Parser<I, Output = &'a str>
+pub fn comments<'a, I>() -> impl Parser<I, Output = ()>
 where
     I: RangeStream<Token = char, Range = &'a str>,
     I::Error: ParseError<I::Token, I::Range, I::Position>,
 {
-    (string("--"), take_while(|ch: char| ch != '\n'))
-        .map(|(_, content): (&str, &str)| content.trim())
+    skip_many((string("--"), take_while(|ch: char| ch != '\n')))
+}
+pub fn insignificants<'a, I>() -> impl Parser<I, Output = ()>
+where
+    I: RangeStream<Token = char, Range = &'a str>,
+    I::Error: ParseError<I::Token, I::Range, I::Position>,
+{
+    skip_many(spaces().or(comments()))
 }
