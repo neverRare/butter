3b135ceabc1a0a79830d6280c3026b49eba8d0db
neverRare <35887047+neverRare@users.noreply.github.com>
2021-03-26 10:16:28 +0800

    implement struct pattern parser

--- core/src/parser_2/pattern.rs
+++ core/src/parser_2/pattern.rs
@@ -1,5 +1,7 @@
 use crate::ast::pattern::ArrayWithRest;
 use crate::ast::pattern::Pattern;
+use crate::ast::pattern::PatternField;
+use crate::ast::pattern::StructPattern;
 use crate::parser_2::ident_keyword::ident;
 use crate::parser_2::ident_keyword::keyword;
 use crate::parser_2::lex;
@@ -53,6 +55,61 @@ where
             None => Pattern::Array(left),
         })
 }
+fn field<'a, I>() -> impl Parser<I, Output = PatternField<'a>>
+where
+    I: RangeStream<Token = char, Range = &'a str>,
+    I::Error: ParseError<I::Token, I::Range, I::Position>,
+{
+    (lex(ident()), optional((lex(char('=')), pattern()))).map(|(name, pattern)| {
+        let pattern = match pattern {
+            Some((_, pattern)) => pattern,
+            None => Pattern::Var(name),
+        };
+        PatternField {
+            name,
+            content: pattern,
+        }
+    })
+}
+fn fields<'a, I>() -> impl Parser<I, Output = Vec<PatternField<'a>>>
+where
+    I: RangeStream<Token = char, Range = &'a str>,
+    I::Error: ParseError<I::Token, I::Range, I::Position>,
+{
+    sep_by(field(), lex(char(',')))
+}
+fn record<'a, I>() -> impl Parser<I, Output = StructPattern<'a>>
+where
+    I: RangeStream<Token = char, Range = &'a str>,
+    I::Error: ParseError<I::Token, I::Range, I::Position>,
+{
+    (
+        lex(char('(')),
+        fields(),
+        optional((rest(), optional((lex(char(',')), fields())))),
+        lex(char(')')),
+    )
+        .map(|(_, left, rest_right, _)| match rest_right {
+            Some((rest, right)) => {
+                let fields = match right {
+                    Some((_, mut right)) => {
+                        let mut fields = left;
+                        fields.append(&mut right);
+                        fields
+                    }
+                    None => left,
+                };
+                StructPattern {
+                    fields,
+                    rest: Some(Box::new(rest)),
+                }
+            }
+            None => StructPattern {
+                fields: left,
+                rest: None,
+            },
+        })
+}
 fn pattern_<'a, I>() -> impl Parser<I, Output = Pattern<'a>>
 where
     I: RangeStream<Token = char, Range = &'a str>,
@@ -60,6 +117,7 @@ where
 {
     choice((
         array(),
+        record().map(Pattern::Struct),
         attempt(lex(keyword("_"))).map(|_| Pattern::Ignore),
         lex(ident()).map(|ident| Pattern::Var(ident)),
     ))
