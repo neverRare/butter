983bb7a8e157f63a3f390f77a9e43a0aa83a7e7c
neverRare <35887047+neverRare@users.noreply.github.com>
2021-03-27 07:43:41 +0800

    fix and refactor pattern parser

--- core/src/parser/pattern.rs
+++ core/src/parser/pattern.rs
@@ -5,11 +5,13 @@ use crate::parser::ident_keyword::ident;
 use crate::parser::ident_keyword::keyword;
 use crate::parser::lex;
 use combine::attempt;
+use combine::between;
 use combine::choice;
 use combine::optional;
 use combine::parser;
 use combine::parser::char::char;
 use combine::sep_by;
+use combine::sep_end_by;
 use combine::ParseError;
 use combine::Parser;
 use combine::RangeStream;
@@ -17,45 +19,56 @@ use std::collections::HashMap;
 
 // TODO: handle trailing comma
 
-fn elements<'a, I>() -> impl Parser<I, Output = Vec<Pattern<'a>>>
+fn optional_rest<'a, I, EP, RP, C>(
+    left: char,
+    right: char,
+    element: fn() -> EP,
+    rest: fn() -> RP,
+) -> impl Parser<I, Output = (C, Option<(RP::Output, C)>)>
 where
     I: RangeStream<Token = char, Range = &'a str>,
     I::Error: ParseError<I::Token, I::Range, I::Position>,
+    EP: Parser<I>,
+    RP: Parser<I>,
+    C: Extend<EP::Output> + Default,
 {
-    sep_by(pattern(), lex(char(',')))
-}
-fn rest<'a, I>() -> impl Parser<I, Output = Pattern<'a>>
-where
-    I: RangeStream<Token = char, Range = &'a str>,
-    I::Error: ParseError<I::Token, I::Range, I::Position>,
-{
-    (lex(char('*')), pattern()).map(|(_, pattern)| pattern)
+    let no_rest = move || sep_by(element(), lex(char(',')));
+    let have_rest = move || {
+        (
+            sep_end_by(element(), lex(char(','))),
+            (lex(char('*'))),
+            rest(),
+            optional((lex(char(',')), no_rest())),
+        )
+            .map(|(left, _, rest, right)| {
+                let right = match right {
+                    Some((_, right)) => right,
+                    None => Default::default(),
+                };
+                (left, rest, right)
+            })
+    };
+    let middle = move || {
+        choice((
+            attempt(no_rest()).map(|collection| (collection, None)),
+            have_rest().map(|(left, rest, right)| (left, Some((rest, right)))),
+        ))
+    };
+    between(lex(char(left)), lex(char(right)), middle())
 }
 fn array<'a, I>() -> impl Parser<I, Output = Pattern<'a>>
 where
     I: RangeStream<Token = char, Range = &'a str>,
     I::Error: ParseError<I::Token, I::Range, I::Position>,
 {
-    (
-        lex(char('[')),
-        elements(),
-        optional((rest(), optional((lex(char(',')), elements())))),
-        lex(char(']')),
-    )
-        .map(|(_, left, rest_right, _)| match rest_right {
-            Some((rest, right)) => {
-                let right = match right {
-                    Some((_, right)) => right,
-                    None => vec![],
-                };
-                Pattern::ArrayWithRest(ArrayWithRest {
-                    left,
-                    rest: Box::new(rest),
-                    right,
-                })
-            }
-            None => Pattern::Array(left),
-        })
+    optional_rest('[', ']', pattern, pattern).map(|(left, rest_right)| match rest_right {
+        Some((rest, right)) => Pattern::ArrayWithRest(ArrayWithRest {
+            left,
+            rest: Box::new(rest),
+            right,
+        }),
+        None => Pattern::Array(left),
+    })
 }
 fn field<'a, I>() -> impl Parser<I, Output = (&'a str, Pattern<'a>)>
 where
@@ -70,45 +83,26 @@ where
         (name, pattern)
     })
 }
-fn fields<'a, I>() -> impl Parser<I, Output = HashMap<&'a str, Pattern<'a>>>
-where
-    I: RangeStream<Token = char, Range = &'a str>,
-    I::Error: ParseError<I::Token, I::Range, I::Position>,
-{
-    // TODO: handle duplicated name as error
-    sep_by(field(), lex(char(',')))
-}
 fn record<'a, I>() -> impl Parser<I, Output = StructPattern<'a>>
 where
     I: RangeStream<Token = char, Range = &'a str>,
     I::Error: ParseError<I::Token, I::Range, I::Position>,
 {
-    (
-        lex(char('(')),
-        fields(),
-        optional((rest(), optional((lex(char(',')), fields())))),
-        lex(char(')')),
-    )
-        .map(|(_, left, rest_right, _)| match rest_right {
-            Some((rest, right)) => {
-                let fields = match right {
-                    Some((_, right)) => {
-                        let mut fields = left;
-                        fields.extend(right);
-                        fields
-                    }
-                    None => left,
-                };
-                StructPattern {
-                    fields,
-                    rest: Some(Box::new(rest)),
-                }
+    // TODO: handle duplicate name as error
+    optional_rest('(', ')', field, pattern).map(|(left, rest_right)| match rest_right {
+        Some((rest, right)) => {
+            let mut fields: HashMap<_, _> = left;
+            fields.extend(right);
+            StructPattern {
+                fields,
+                rest: Some(Box::new(rest)),
             }
-            None => StructPattern {
-                fields: left,
-                rest: None,
-            },
-        })
+        }
+        None => StructPattern {
+            fields: left,
+            rest: None,
+        },
+    })
 }
 fn pattern_<'a, I>() -> impl Parser<I, Output = Pattern<'a>>
 where
