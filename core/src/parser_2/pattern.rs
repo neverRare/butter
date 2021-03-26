00b8aa3fc889f5bc6c4e752093212cd577fb59b9
neverRare <35887047+neverRare@users.noreply.github.com>
2021-03-26 09:56:48 +0800

    update array pattern parser to handle rest

--- core/src/parser_2/pattern.rs
+++ core/src/parser_2/pattern.rs
@@ -1,10 +1,11 @@
+use crate::ast::pattern::ArrayWithRest;
 use crate::ast::pattern::Pattern;
 use crate::parser_2::ident_keyword::ident;
 use crate::parser_2::ident_keyword::keyword;
 use crate::parser_2::lex;
 use combine::attempt;
-use combine::between;
 use combine::choice;
+use combine::optional;
 use combine::parser;
 use combine::parser::char::char;
 use combine::sep_by;
@@ -12,16 +13,45 @@ use combine::ParseError;
 use combine::Parser;
 use combine::RangeStream;
 
-fn array<'a, I>() -> impl Parser<I, Output = Vec<Pattern<'a>>>
+fn elements<'a, I>() -> impl Parser<I, Output = Vec<Pattern<'a>>>
 where
     I: RangeStream<Token = char, Range = &'a str>,
     I::Error: ParseError<I::Token, I::Range, I::Position>,
 {
-    between(
+    sep_by(pattern(), lex(char(',')))
+}
+fn rest<'a, I>() -> impl Parser<I, Output = Pattern<'a>>
+where
+    I: RangeStream<Token = char, Range = &'a str>,
+    I::Error: ParseError<I::Token, I::Range, I::Position>,
+{
+    (lex(char('*')), pattern()).map(|(_, pattern)| pattern)
+}
+fn array<'a, I>() -> impl Parser<I, Output = Pattern<'a>>
+where
+    I: RangeStream<Token = char, Range = &'a str>,
+    I::Error: ParseError<I::Token, I::Range, I::Position>,
+{
+    (
         lex(char('[')),
+        elements(),
+        optional((rest(), optional((lex(char(',')), elements())))),
         lex(char(']')),
-        sep_by(pattern(), lex(char(','))),
     )
+        .map(|(_, left, rest_right, _)| match rest_right {
+            Some((rest, right)) => {
+                let right = match right {
+                    Some((_, right)) => right,
+                    None => vec![],
+                };
+                Pattern::ArrayWithRest(ArrayWithRest {
+                    left,
+                    rest: Box::new(rest),
+                    right,
+                })
+            }
+            None => Pattern::Array(left),
+        })
 }
 fn pattern_<'a, I>() -> impl Parser<I, Output = Pattern<'a>>
 where
@@ -29,7 +59,7 @@ where
     I::Error: ParseError<I::Token, I::Range, I::Position>,
 {
     choice((
-        array().map(Pattern::Array),
+        array(),
         attempt(lex(keyword("_"))).map(|_| Pattern::Ignore),
         lex(ident()).map(|ident| Pattern::Var(ident)),
     ))
