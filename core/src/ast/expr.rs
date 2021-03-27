a94ea64e81c59d4856c35ae2161c4593647bdb65
neverRare <35887047+neverRare@users.noreply.github.com>
2021-03-27 08:56:11 +0800

    update ast
    add index operator
    flatten operators
    add place expression for assignment
    add parallel assignmanent

--- core/src/ast/expr.rs
+++ core/src/ast/expr.rs
@@ -6,11 +6,12 @@ use crate::ast::expr::control_flow::For;
 use crate::ast::expr::control_flow::Fun;
 use crate::ast::expr::control_flow::If;
 use crate::ast::expr::control_flow::While;
-use crate::ast::expr::operator::BinaryExpr;
+use crate::ast::expr::operator::Assign;
+use crate::ast::expr::operator::Binary;
 use crate::ast::expr::operator::Call;
+use crate::ast::expr::operator::Index;
 use crate::ast::expr::operator::Property;
 use crate::ast::expr::operator::Slice;
-use crate::ast::expr::operator::UnaryExpr;
 use crate::ast::expr::range::Range;
 
 pub mod compound;
@@ -24,7 +25,7 @@ pub enum Expr<'a> {
     False,
     Null,
 
-    Ident(&'a str),
+    Var(&'a str),
     Char(u8),
     Str(Vec<u8>),
     UInt(u64),
@@ -34,8 +35,33 @@ pub enum Expr<'a> {
     Continue(Option<&'a str>),
     Return(Option<Box<Expr<'a>>>),
 
-    Unary(UnaryExpr<'a>),
-    Binary(BinaryExpr<'a>),
+    Plus(Box<Expr<'a>>),
+    Minus(Box<Expr<'a>>),
+    Ref(Box<Expr<'a>>),
+    Not(Box<Expr<'a>>),
+    Clone(Box<Expr<'a>>),
+
+    Add(Binary<'a>),
+    Sub(Binary<'a>),
+    Multiply(Binary<'a>),
+    Div(Binary<'a>),
+    FloorDiv(Binary<'a>),
+    Mod(Binary<'a>),
+    And(Binary<'a>),
+    Or(Binary<'a>),
+    LazyAnd(Binary<'a>),
+    LazyOr(Binary<'a>),
+    Equal(Binary<'a>),
+    NotEqual(Binary<'a>),
+    Greater(Binary<'a>),
+    GreaterEqual(Binary<'a>),
+    Less(Binary<'a>),
+    LessEqual(Binary<'a>),
+    Concatenate(Binary<'a>),
+    NullOr(Binary<'a>),
+
+    Assign(Assign<'a>),
+    ParallelAssign(Vec<Assign<'a>>),
 
     Array(Vec<Element<'a>>),
     ArrayRange(Range<'a>),
@@ -43,6 +69,8 @@ pub enum Expr<'a> {
 
     Property(Property<'a>),
     OptionalProperty(Property<'a>),
+    Index(Index<'a>),
+    OptionalIndex(Index<'a>),
     Slice(Slice<'a>),
     OptionalSlice(Slice<'a>),
     Call(Call<'a>),
@@ -53,4 +81,11 @@ pub enum Expr<'a> {
     For(For<'a>),
     While(While<'a>),
     Loop(Block<'a>),
+}
+#[derive(Debug, PartialEq, Clone)]
+pub enum PlaceExpr<'a> {
+    Var(&'a str),
+    Ref(Box<Expr<'a>>),
+    Property(Property<'a>),
+    Index(Index<'a>),
 }
--- core/src/ast/expr/operator.rs
+++ core/src/ast/expr/operator.rs
@@ -1,52 +1,27 @@
 use crate::ast::expr::compound::Arg;
 use crate::ast::expr::range::Range;
 use crate::ast::expr::Expr;
+use crate::ast::expr::PlaceExpr;
 
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-pub enum Unary {
-    Plus,
-    Minus,
-    Ref,
-    Not,
-    Clone,
-}
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-pub enum Binary {
-    Add,
-    Sub,
-    Multiply,
-    Div,
-    FloorDiv,
-    Mod,
-    And,
-    Or,
-    LazyAnd,
-    LazyOr,
-    Equal,
-    NotEqual,
-    Greater,
-    GreaterEqual,
-    Less,
-    LessEqual,
-    Concatenate,
-    NullOr,
-    Assign,
-}
 #[derive(Debug, PartialEq, Clone)]
-pub struct UnaryExpr<'a> {
-    pub op: Unary,
-    pub expr: Box<Expr<'a>>,
-}
-#[derive(Debug, PartialEq, Clone)]
-pub struct BinaryExpr<'a> {
-    pub op: Unary,
+pub struct Binary<'a> {
     pub left: Box<Expr<'a>>,
     pub right: Box<Expr<'a>>,
+}
+#[derive(Debug, PartialEq, Clone)]
+pub struct Assign<'a> {
+    pub place: Box<PlaceExpr<'a>>,
+    pub expr: Box<Expr<'a>>,
 }
 #[derive(Debug, PartialEq, Clone)]
 pub struct Property<'a> {
     pub expr: Box<Expr<'a>>,
     pub name: &'a str,
+}
+#[derive(Debug, PartialEq, Clone)]
+pub struct Index<'a> {
+    pub expr: Box<Expr<'a>>,
+    pub index: Box<Expr<'a>>,
 }
 #[derive(Debug, PartialEq, Clone)]
 pub struct Slice<'a> {
