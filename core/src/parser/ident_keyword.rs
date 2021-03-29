ac449f4ca24dda65357f85a9f280fb1f6a81eb92
neverRare <35887047+neverRare@users.noreply.github.com>
2021-03-29 10:05:42 +0800

    update ident or keyword parser

--- core/src/parser/ident_keyword.rs
+++ core/src/parser/ident_keyword.rs
@@ -1,5 +1,7 @@
 use combine::error::StreamError;
-use combine::parser::range::take_while1;
+use combine::parser::range::recognize;
+use combine::parser::range::take_while;
+use combine::satisfy;
 use combine::stream::StreamErrorFor;
 use combine::ParseError;
 use combine::Parser;
@@ -10,7 +12,9 @@ where
     I: RangeStream<Token = char, Range = &'a str>,
     I::Error: ParseError<I::Token, I::Range, I::Position>,
 {
-    take_while1(|ch: char| ch.is_alphanumeric() || ch == '_')
+    let rest = |ch: char| ch.is_alphanumeric() || ch == '_';
+    let start = move |ch: char| rest(ch) && !('0'..='9').contains(&ch);
+    recognize((satisfy(start), take_while(rest)))
 }
 pub fn keyword<'a, I>(keyword: &'static str) -> impl Parser<I, Output = &'a str>
 where
@@ -37,4 +41,18 @@ where
         }
         _ => Ok(ident),
     })
+}
+#[cfg(test)]
+mod test {
+    use crate::parser::ident_keyword::ident_or_keyword;
+    use combine::EasyParser;
+
+    #[test]
+    fn ident() {
+        assert_eq!(ident_or_keyword().easy_parse("foo"), Ok(("foo", "")))
+    }
+    #[test]
+    fn non_ident() {
+        assert!(ident_or_keyword().easy_parse("12").is_err())
+    }
 }
