77140f368625916287546e97e74033383d38e58c
neverRare <35887047+neverRare@users.noreply.github.com>
2021-04-11 11:03:50 +0800

    another whoops

--- core/src/parser/expr.rs
+++ core/src/parser/expr.rs
@@ -40,7 +40,12 @@ where
     I::Error: ParseError<I::Token, I::Range, I::Position>,
 {
     choice((
-        (attempt(param_arrow()), expr(0)).map(|(param, body)| Expr::Fun(Fun { param, body })),
+        (attempt(param_arrow()), expr(0)).map(|(param, body)| {
+            Expr::Fun(Fun {
+                param,
+                body: Box::new(body),
+            })
+        }),
         attempt(range()).map(Expr::ArrayRange),
         array().map(Expr::Array),
         attempt(between(lex(char('(')), lex(char(')')), expr(0))),
