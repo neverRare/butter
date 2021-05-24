060fd96fee63353e2fa511e4f055f557adf686ec
neverRare <35887047+neverRare@users.noreply.github.com>
2021-05-24 08:13:55 +0800

    some updates to type system

--- type-system/src/cons.rs
+++ type-system/src/cons.rs
@@ -1,6 +1,9 @@
 use crate::fmt_intersperse;
+use crate::HashSet;
+use crate::Subs;
 use crate::Type;
 use crate::Var;
+use std::array::IntoIter as ArrayIntoIter;
 use std::collections::HashMap;
 use std::fmt::Display;
 use std::fmt::Formatter;
@@ -11,11 +14,108 @@ pub(super) enum Cons<'a> {
     Unit,
     Num,
     Bool,
+    Ref(Box<Type<'a>>),
+    RefMut(Box<Type<'a>>),
     Array(Box<Type<'a>>),
     Record(RecordCons<'a>),
     Fun(FunCons<'a>),
     Tuple(Box<[Type<'a>]>),
     Union(Union<'a>),
+}
+impl<'a> Cons<'a> {
+    pub fn free_vars(&self) -> HashSet<Var<'a>> {
+        match self {
+            Self::Unit | Self::Num | Self::Bool => HashSet::new(),
+            Self::Ref(ty) | Self::RefMut(ty) | Self::Array(ty) => ty.free_vars(),
+            Self::Fun(fun) => ArrayIntoIter::new([&fun.param, &fun.result])
+                .map(AsRef::as_ref)
+                .flat_map(Type::free_vars)
+                .collect(),
+            Self::Record(record) => record
+                .fields
+                .values()
+                .flat_map(Type::free_vars)
+                .chain(record.rest.iter().copied())
+                .collect(),
+            Self::Tuple(tuple) => tuple.iter().flat_map(Type::free_vars).collect(),
+            Self::Union(union) => union
+                .union
+                .values()
+                .flat_map(Type::free_vars)
+                .chain(union.rest.iter().copied())
+                .collect(),
+        }
+    }
+    // NOTE: this contains panics, should they be handled as Result?
+    pub fn substitute(&mut self, subs: &Subs<'a>) {
+        match self {
+            Self::Unit | Self::Num | Self::Bool => (),
+            Self::Ref(ty) | Self::RefMut(ty) | Self::Array(ty) => ty.substitute(subs),
+            Self::Fun(fun) => {
+                fun.param.substitute(subs);
+                fun.result.substitute(subs);
+            }
+            Self::Record(record) => {
+                for (_, ty) in &mut record.fields {
+                    ty.substitute(subs);
+                }
+                if let Some(var) = &record.rest {
+                    let var = *var;
+                    match subs.get(&var) {
+                        Some(Type::Var(new_var)) => {
+                            record.rest = Some(*new_var);
+                        }
+                        Some(Type::Cons(Self::Record(new_rest))) => {
+                            let new_fields = &new_rest.fields;
+                            record.fields.reserve(new_fields.len());
+                            for (key, ty) in new_fields {
+                                if record.fields.contains_key(key) {
+                                    panic!("overlapping key {}", key);
+                                } else {
+                                    record.fields.insert(*key, ty.clone());
+                                }
+                            }
+                            record.rest = new_rest.rest;
+                        }
+                        Some(_) => panic!("substituted record rest var as non-record type"),
+                        None => (),
+                    }
+                }
+            }
+            Self::Tuple(tuple) => {
+                for ty in &mut tuple[..] {
+                    ty.substitute(subs);
+                }
+            }
+            Self::Union(union) => {
+                for (_, ty) in &mut union.union {
+                    ty.substitute(subs);
+                }
+                if let Some(var) = &union.rest {
+                    let var = *var;
+                    match subs.get(&var) {
+                        Some(Type::Var(new_var)) => {
+                            union.rest = Some(*new_var);
+                        }
+                        Some(Type::Cons(Self::Union(new_rest))) => {
+                            let new_fields = &new_rest.union;
+                            union.union.reserve(new_fields.len());
+                            for (tag, ty) in new_fields {
+                                if union.union.contains_key(tag) {
+                                    panic!("overlapping tag {}", tag);
+                                } else {
+                                    union.union.insert(*tag, ty.clone());
+                                }
+                            }
+                            union.rest = new_rest.rest;
+                        }
+                        Some(_) => panic!("substituted union rest var as non-union type"),
+                        None => (),
+                    }
+                }
+            }
+        }
+    }
 }
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub(super) struct RecordCons<'a> {
@@ -39,6 +139,8 @@ impl<'a> Display for Cons<'a> {
             Self::Unit => write!(fmt, "unit"),
             Self::Num => write!(fmt, "number"),
             Self::Bool => write!(fmt, "boolean"),
+            Self::Ref(ty) => write!(fmt, "&{}", ty),
+            Self::RefMut(ty) => write!(fmt, "&mut {}", ty),
             Self::Array(ty) => write!(fmt, "[{}]", ty),
             Self::Record(record) => record.fmt(fmt),
             Self::Fun(fun) => fun.fmt(fmt),
--- type-system/src/lib.rs
+++ type-system/src/lib.rs
@@ -4,10 +4,11 @@ use std::collections::HashSet;
 use std::fmt::Display;
 use std::fmt::Formatter;
 use std::fmt::Result as FmtResult;
+use std::iter::once;
 
 mod cons;
 
-#[derive(Debug, PartialEq, Eq, Clone, Hash)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 struct Var<'a> {
     name: &'a str,
     id: u32,
@@ -17,23 +18,84 @@ enum Type<'a> {
     Var(Var<'a>),
     Cons(Cons<'a>),
 }
+impl<'a> Type<'a> {
+    fn free_vars(&self) -> HashSet<Var<'a>> {
+        match self {
+            Self::Var(var) => once(*var).collect(),
+            Self::Cons(cons) => cons.free_vars(),
+        }
+    }
+    fn substitute(&mut self, subs: &Subs<'a>) {
+        match self {
+            Self::Var(var) => {
+                if let Some(ty) = subs.get(var) {
+                    *self = ty.clone()
+                }
+            }
+            Self::Cons(cons) => cons.substitute(subs),
+        }
+    }
+}
 #[derive(Debug, PartialEq, Eq, Clone)]
 struct Scheme<'a> {
     for_all: HashSet<Var<'a>>,
     ty: Type<'a>,
 }
-struct Subs<'a>(HashMap<Var<'a>, Type<'a>>);
-impl<'a> Subs<'a> {
-    fn hashmap(&self) -> &HashMap<Var<'a>, Type<'a>> {
-        let Self(map) = self;
-        map
+impl<'a> Scheme<'a> {
+    fn free_vars(&self) -> HashSet<Var<'a>> {
+        self.ty
+            .free_vars()
+            .into_iter()
+            .filter(|var| !self.for_all.contains(var))
+            .collect()
+    }
+    fn substitute(&mut self, subs: &Subs<'a>) {
+        let subs = subs
+            .iter()
+            .filter_map(|(var, ty)| {
+                if self.for_all.contains(var) {
+                    None
+                } else {
+                    Some((*var, ty.clone()))
+                }
+            })
+            .collect();
+        self.ty.substitute(&subs)
     }
 }
+type Subs<'a> = HashMap<Var<'a>, Type<'a>>;
 struct Env<'a>(HashMap<Var<'a>, Scheme<'a>>);
 impl<'a> Env<'a> {
     fn hashmap(&self) -> &HashMap<Var<'a>, Scheme<'a>> {
         let Self(map) = self;
         map
+    }
+    fn hashmap_mut(&mut self) -> &mut HashMap<Var<'a>, Scheme<'a>> {
+        let Self(map) = self;
+        map
+    }
+    fn remove(&mut self, var: Var<'a>) {
+        self.hashmap_mut().remove(&var);
+    }
+    fn free_vars(&self) -> HashSet<Var<'a>> {
+        self.hashmap()
+            .values()
+            .flat_map(Scheme::free_vars)
+            .collect()
+    }
+    fn substitute(&mut self, subs: &Subs<'a>) {
+        for (_, ty) in self.hashmap_mut() {
+            ty.substitute(subs);
+        }
+    }
+    fn generalize(&self, ty: Type<'a>) -> Scheme<'a> {
+        let env_free_vars = self.free_vars();
+        let for_all = ty
+            .free_vars()
+            .into_iter()
+            .filter(|var| !env_free_vars.contains(var))
+            .collect();
+        Scheme { for_all, ty }
     }
 }
 impl<'a> Display for Var<'a> {
