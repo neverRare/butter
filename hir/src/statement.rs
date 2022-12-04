452461594429ff84ea450907919a889e7090ff33
neverRare <contactneverrare@gmail.com>
2022-12-04 06:19:07 +0000

    partially implement pretty printer for statements

--- hir/src/statement.rs
+++ hir/src/statement.rs
@@ -1,7 +1,8 @@
 use crate::{
     expr::{Expr, Fun},
     pattern::Pattern,
-    Atom,
+    pretty_print::{line, PrettyPrint, PrettyPrintTree},
+    Atom, PrettyPrintType,
 };
 
 #[derive(Debug, PartialEq, Clone)]
@@ -10,10 +11,28 @@ pub enum Statement<T> {
     FunDeclare(FunDeclare<T>),
     Expr(Expr<T>),
 }
+impl<T: PrettyPrintType> PrettyPrint for Statement<T> {
+    fn to_pretty_print(&self) -> Box<dyn PrettyPrintTree> {
+        match self {
+            Statement::Declare(declare) => declare.to_pretty_print(),
+            Statement::FunDeclare(_) => todo!(),
+            Statement::Expr(expr) => expr.to_pretty_print(),
+        }
+    }
+}
 #[derive(Debug, PartialEq, Clone)]
 pub struct Declare<T> {
     pub pattern: Pattern<T>,
     pub expr: Expr<T>,
+}
+impl<T: PrettyPrintType> PrettyPrint for Declare<T> {
+    fn to_pretty_print(&self) -> Box<dyn PrettyPrintTree> {
+        line([
+            self.pattern.to_pretty_print(),
+            Box::new(" = ".to_string()),
+            self.expr.to_pretty_print(),
+        ])
+    }
 }
 #[derive(Debug, PartialEq, Clone)]
 pub struct FunDeclare<T> {
